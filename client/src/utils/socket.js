77c89861858360eff7c2a4ee18e2593df9ba6559
ReturnofMatrix <roshanbhogan007@gmail.com>
2025-11-19 20:55:46 +0530

    trying with the help of cursor.

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,14 +1,14 @@
 // utils/socket.js
 import { io } from "socket.io-client";
 
-const PROD_URL = "https://messagingapp-ej7a.onrender.com";
+const PROD_URL = "https://messagingapp-fzs6.onrender.com";
 const DEV_URL = "http://localhost:4000";
 
 const socket = io(
   process.env.NODE_ENV === "production" ? PROD_URL : DEV_URL,
   {
     withCredentials: true,
-    transports: ["polling", "websocket"]
+    transports: [ "websocket"]
   }
 );
 
